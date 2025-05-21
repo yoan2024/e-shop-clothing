a78cacf52166f254354af8ca6be2cccf8122395c
Yoan Sebastian <yoansebastianramirezgarcia@gmail.com>
2025-05-21 18:43:20 -0500

    agregando el funcionalida al componenete tendencia, mejores productos

---
+++ src/component/Informacion.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Informacion = () => {
+  return <div>Informacion</div>;
+};
+
+export default Informacion;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hero from "../component/Hero";
+import Informacion from "../component/Informacion";
+import Tendencia from "../component/tendencia";
 
 const Home = () => {
-  return <div className="w-full min-h-screen bg-blue-400">Home</div>;
+  return (
+    <div className="w-full mt-14  min-h-screen bg-blue-400">
+      <Hero />
+      <Tendencia />
+      <Informacion />
+    </div>
+  );
 };
 
 export default Home;
