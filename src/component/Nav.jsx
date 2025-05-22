22418816aeb630ece3bec791118313ec6b4f0724
Yoan Sebastian <yoansebastianramirezgarcia@gmail.com>
2025-05-22 18:48:29 -0500

    agregando categoria y relevancia ropa en home

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="w-full bg-white shadow-md p-3 fixed top-0 left-0 z-50">
-      <div className="flex  w-11/12 flex-row justify-between items-center">
+    <div className="w-full bg-white/90 shadow-md flex flex-row justify-center p-3 fixed top-0 left-0 z-50">
+      <div className="flex  w-11/12  flex-row  justify-between items-center">
         <div className="flex flex-row gap-2 text-xl">
           <div>MAN</div>
           <div>WOMAN</div>
@@ -14,7 +14,7 @@ const Nav = () => {
         <div>
           <div className="text-2xl font-semibold">Trendora</div>
         </div>
-        <div className="flex flex-row  gap-4">
+        <div className="flex flex-row   gap-4">
           <div className="w-5 h-5">
             <img src="/images/lupa.png" alt="" className="w-full h-full" />
           </div>
---
+++ src/component/ProductsShow.jsx
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from "react";
+import { useProducts } from "../context/ContextProducts";
+
+const ProductsShow = () => {
+  const { products, setProducts } = useProducts();
+  const [productHombre, setProductsHombre] = useState([]);
+  const [productMujer, setProductsMujer] = useState([]);
+  const [electronics, setElectronics] = useState([]);
+  const [joyeria, setJoyeria] = useState([]);
+
+  useEffect(() => {
+    async function name(params) {
+      const productos = await products;
+      if (productos) {
+        const productsMujer = [...productos].filter(
+          (p) => p.category === "women's clothing"
+        );
+        if (productsMujer) {
+          setProductsMujer(productsMujer);
+        }
+        const productsHombre = [...productos].filter(
+          (p) => p.category === "men's clothing"
+        );
+        if (productsHombre) {
+          setProductsHombre(productsHombre);
+        }
+        const electronicos = [...productos].filter(
+          (p) => p.category === "electronics"
+        );
+
+        if (electronicos) {
+          setElectronics(electronicos);
+        }
+
+        const jowery = [...productos].filter((p) => p.category === "jewelery");
+
+        if (jowery) {
+          setJoyeria(jowery);
+        }
+      }
+    }
+
+    name();
+  }, [products]);
+
+  console.log("mujer", productMujer);
+  console.log("hombre", productHombre);
+  if (!productHombre) return <li>Cargando...</li>;
+  if (!productMujer) return <li>Cargando...</li>;
+
+  return (
+    <div className="w-full flex items-center flex-col   mt-28 border-solid border-t-2 border-slate-200 ">
+      <div className="text-3xl  self-center">
+        productos relevantes de hombre
+      </div>
+
+      <div className="flex flex-wrap justify-center flex-row items-center gap-5">
+        {productHombre.map((p) => (
+          <div
+            key={p.id}
+            className="w-48 h-64 flex flex-col items-center text-center transition-transform duration-300 transform hover:scale-105 hover:shadow-lg border rounded-xl p-2"
+          >
+            <div className="bg-white w-full h-40 flex items-center justify-center overflow-hidden rounded-md">
+              <img
+                src={p.image}
+                alt={p.title}
+                className="max-h-full max-w-full transition-transform duration-300 transform hover:scale-110"
+              />
+            </div>
+            <div className="mt-2 text-sm font-medium line-clamp-2 h-10">
+              {p.title}
+            </div>
+            <div className="text-lg font-bold">${p.price}</div>
+          </div>
+        ))}
+      </div>
+      <div className="text-3xl  self-center mt-10">
+        productos relevantes de Mujer
+      </div>
+      <div className="flex flex-wrap justify-center flex-row items-center gap-5">
+        {productMujer.map((p) => (
+          <div
+            key={p.id}
+            className="w-48 h-64 flex flex-col items-center text-center transition-transform duration-300 transform hover:scale-105 hover:shadow-lg border rounded-xl p-2"
+          >
+            <div className="bg-white w-full h-40 flex items-center justify-center overflow-hidden rounded-md">
+              <img
+                src={p.image}
+                alt={p.title}
+                className="max-h-full max-w-full transition-transform duration-300 transform hover:scale-110"
+              />
+            </div>
+            <div className="mt-2 text-sm font-medium line-clamp-2 h-10">
+              {p.title}
+            </div>
+            <div className="text-lg font-bold">${p.price}</div>
+          </div>
+        ))}
+      </div>
+      <div className="text-3xl  self-center mt-10">
+        productos relevantes de electronics
+      </div>
+      <div className="flex flex-wrap justify-center flex-row items-center gap-5">
+        {electronics.map((p) => (
+          <div
+            key={p.id}
+            className="w-48 h-64 flex flex-col items-center text-center transition-transform duration-300 transform hover:scale-105 hover:shadow-lg border rounded-xl p-2"
+          >
+            <div className="bg-white w-full h-40 flex items-center justify-center overflow-hidden rounded-md">
+              <img
+                src={p.image}
+                alt={p.title}
+                className="max-h-full max-w-full transition-transform duration-300 transform hover:scale-110"
+              />
+            </div>
+            <div className="mt-2 text-sm font-medium line-clamp-2 h-10">
+              {p.title}
+            </div>
+            <div className="text-lg font-bold">${p.price}</div>
+          </div>
+        ))}
+      </div>
+      <div className="text-3xl  self-center mt-10">
+        productos relevantes de jowery
+      </div>
+      <div className="flex flex-wrap justify-center flex-row items-center gap-5">
+        {joyeria.map((p) => (
+          <div
+            key={p.id}
+            className="w-48 h-64 flex flex-col items-center text-center transition-transform duration-300 transform hover:scale-105 hover:shadow-lg border rounded-xl p-2"
+          >
+            <div className="bg-white w-full h-40 flex items-center justify-center overflow-hidden rounded-md">
+              <img
+                src={p.image}
+                alt={p.title}
+                className="max-h-full max-w-full transition-transform duration-300 transform hover:scale-110"
+              />
+            </div>
+            <div className="mt-2 text-sm font-medium line-clamp-2 h-10">
+              {p.title}
+            </div>
+            <div className="text-lg font-bold">${p.price}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductsShow;
